e254d686a63fe86bfed234f5cd79379e5149acf7
Pratham Karki <prkarki@infinite.com>
2026-08-25 21:34:02 +0545

    exercises on day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+string title = "Learning .net";
+Boolean isComplete = false;
+int priority = 3;
+double x = 1.2 + 2.3;
+var dateOnly = new DateOnly();
+
+
+Console.WriteLine(title);
+Console.WriteLine(isComplete);
+Console.WriteLine(priority);
+Console.WriteLine(dateOnly);
+Console.WriteLine(x);
