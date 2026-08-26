b1ef234684fec75741ade4c8a3c65ec45e24b5be
Pratham Karki <prkarki@infinite.com>
2026-08-26 23:00:19 +0545

    update program using OOP concepts

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,31 +1,66 @@
-﻿
-public class Task
+﻿public abstract class Entity
 {
-    public int Id;
-     public string Description;
-     public string Status;
+    public abstract int Id { get; set; }
+}
+
+class Task : Entity
+{
+
+    private static int nextId = 1;
+     public override int Id { get; set; }
+
+    public string Description;
+
+     private string status;
+     public string Status
+    {
+        get
+        {
+            return status;
+        }
+
+        set
+        {
+            if (value.ToLower() == "new" || value.ToLower() == "pending" || value.ToLower() == "completed")
+            {
+                status = value;
+            }
+            else
+            {
+                throw new Exception("Invalid status");
+            }
+        }
+    }
+
+    public Task(string description, string status)
+    {
+        Id = nextId++;
+        Description = description;
+        Status = status;
+
+
+    }
 }
 class Program
 {
     static List<Task> tasks = new List<Task>();
     static void AddTask()
     {
-        Task task = new Task();
-        Console.Write("Enter ID");
-        task.Id = int.Parse(Console.ReadLine());
-        Console.Write("Enter Description");
-        task.Description = Console.ReadLine();
-        task.Status = "Pending";
+
+        Console.Write("Enter the Description of your task:");
+        string description = Console.ReadLine();
+        Task task = new Task(description, "New");
         tasks.Add(task);
-        Console.WriteLine("Task added");
+        Console.WriteLine("Task is added");
     }
     static void ListTasks()
     {
         foreach (Task task in tasks)
         {
-            Console.WriteLine("ID: " + task.Id);
+            Console.WriteLine("Task ID: " + task.Id);
             Console.WriteLine("Description: " + task.Description);
             Console.WriteLine("Status: " + task.Status);
+            Console.WriteLine();
         }
     }
     static void CompleteTask()
@@ -60,19 +95,40 @@ class Program
     }
 
 
+
+    static void UpdateTaskStatus()
+    {
+        Console.Write("Enter the ID:");
+        int id = int.Parse(Console.ReadLine());
+        foreach (Task task in tasks)
+        {
+            if (task.Id == id)
+            {
+                Console.Write("Enter status New, Pending or Completed");
+                task.Status = Console.ReadLine();
+
+                Console.WriteLine("Status Updated");
+                return;
+            }
+            Console.WriteLine("Task not found.");
+
+        }
+    }
+
+
     static void SearchByStatus()
     {
-        Console.WriteLine("Enter the status i.e Pending or Completed");
-        string Status = Console.ReadLine();
+        Console.Write("Enter the status i.e New, Pending or Completed");
+        string status = Console.ReadLine();
 
         bool searchFound = false;
 
         foreach (Task task in tasks)
         {
 
-            if (task.Status.Equals(Status))
+            if (task.Status.ToLower() == status.ToLower())
             {
-                Console.WriteLine("ID: " + task.Id);
+                Console.WriteLine("Task ID: " + task.Id);
                 Console.WriteLine("Description: " + task.Description);
                 Console.WriteLine("Status: " + task.Status);
                 Console.WriteLine();
@@ -83,13 +139,14 @@ class Program
         }
         if (!searchFound)
         {
-            Console.WriteLine("No task found with provided status: " + Status);
+            Console.WriteLine("No task found with provided status: " + status);
         }
     }
 
 
     static void Main()
     {
+
         while (true)
         {
             Console.WriteLine("1. Add");
@@ -97,7 +154,8 @@ class Program
             Console.WriteLine("3. Complete");
             Console.WriteLine("4. Delete");
             Console.WriteLine("5. Search By Status");
-            Console.WriteLine("6. Exit");
+            Console.WriteLine("6. Update Status");
+            Console.WriteLine("7. Exit");
             Console.Write("Enter choice:");
             int choice = int.Parse(Console.ReadLine());
             switch (choice)
@@ -118,6 +176,9 @@ class Program
                     SearchByStatus();
                     break;
                 case 6:
+                    UpdateTaskStatus();
+                    break;
+                case 7:
                     return;
                 default:
                     Console.WriteLine("Invalid choice");
